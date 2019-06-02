704553dac770fbd23945d4ef347ef50f1b101087
gdbaldw <douglasbaldwin@verizon.net>
2019-06-02 13:41:35 -0400

    Replace bears with yoga_poses

--- app/server.py
+++ app/server.py
@@ -9,10 +9,16 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/rl0gjhhvvamiz0n/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = '''Adho_mukha_svanasana
+Bakasana
+Balasana
+Handstand
+Lotus_position
+Shavasana
+Virabhadrasana_I'''.split()
 path = Path(__file__).parent
 
 app = Starlette()
